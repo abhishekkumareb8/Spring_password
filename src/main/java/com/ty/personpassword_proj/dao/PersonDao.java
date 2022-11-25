9b61b45c2c577af6db0926bdc0e9031e763baa28
abhishekjavadevloper <abhishekjavadeveloper.com>
2022-11-25 17:05:14 +0530

    updated code

--- src/main/java/com/ty/personpassword_proj/dao/PersonDao.java
+++ src/main/java/com/ty/personpassword_proj/dao/PersonDao.java
@@ -1,5 +1,85 @@
 package com.ty.personpassword_proj.dao;
 
-public class PersonDao {
+import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Query;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.ty.personpassword_proj.dto.Application;
+import com.ty.personpassword_proj.dto.User;
+
+@Repository
+public class PersonDao {
+	@Autowired
+	private EntityManagerFactory entityManagerFactory;
+
+	public User saveUser(User user) {
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
+		EntityTransaction entityTransaction = entityManager.getTransaction();
+		entityTransaction.begin();
+		entityManager.persist(user);
+		entityTransaction.commit();
+		return user;
+	}
+	
+	public Application saveApp(Application application) {
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
+		EntityTransaction entityTransaction = entityManager.getTransaction();
+		entityTransaction.begin();
+		entityManager.persist(application);
+		entityTransaction.commit();
+		return application;
+	}
+
+	public User updateUser(User user) {
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
+		EntityTransaction entityTransaction = entityManager.getTransaction();
+		entityTransaction.begin();
+		entityManager.merge(user);
+		entityTransaction.commit();
+		return user;
+	}
+
+	public boolean deleteUser(int id) {
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
+		User user = entityManager.find(User.class, id);
+		EntityTransaction entityTransaction = entityManager.getTransaction();
+		entityTransaction.begin();
+		entityManager.remove(user);
+		entityTransaction.commit();
+
+		return true;
+	}
+
+	public User getUserById(int id) {
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
+		return entityManager.find(User.class, id);
+	}
+
+	public List<User> getUser() {
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
+		Query query = entityManager.createQuery("select u from User u", User.class);
+		List<User> employee = query.getResultList();
+		return employee;
+	}
+	
+	public List<Application> getapp() {
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
+		Query query = entityManager.createQuery("select u from Application u", Application.class);
+		return query.getResultList();
+	
+	}
+
+	public User getUserEmail(String email) {
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
+		Query query = entityManager.createQuery("Select u from Employee u where u.email=?1", User.class)
+				.setParameter(1, email);
+
+		return (User) query.getSingleResult();
+	}
 }
--- src/main/java/com/ty/personpassword_proj/service/PersonService.java
+++ src/main/java/com/ty/personpassword_proj/service/PersonService.java
@@ -1,8 +1,33 @@
+
 package com.ty.personpassword_proj.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.ty.personpassword_proj.dao.PersonDao;
+import com.ty.personpassword_proj.dto.Application;
+import com.ty.personpassword_proj.dto.User;
 
 @Service
 public class PersonService {
+	
+	@Autowired
+	PersonDao dao;
+	
+	
+	public User saveuser(User user ) {
+		dao.saveUser(user);
+		return user;
+	}
+	
+	public Application saveapp(Application application) {
+		dao.saveApp(application);
+		return application;
+	}
+	public User user(User user)
+	{
+		return dao.getUserEmail(user.getEmail());
+	}
+	
 
 }
